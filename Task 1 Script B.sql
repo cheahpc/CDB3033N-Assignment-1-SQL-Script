06b85d6e722c87ec392685b94d44002ea153f96a
Cheah Pin Chee <mr.chee37@gmail.com>
2024-02-25 14:04:10 +0800

    Task B prototype

--- Task 1 Script B.sql
+++ Task 1 Script B.sql
@@ -1,21 +1,37 @@
-CREATE VIEW student_course_avg_view AS
-SELECT e.term,
-       e.lineno,
-       e.sid,
-       s.lname,
-       s.fname,
-       ROUND(SUM(comp.weight * sc.points) / SUM(comp.weight), 0) AS course_avg
-FROM enrolls e
-JOIN scores sc ON e.sid = sc.sid AND e.term = sc.term AND e.lineno = sc.lineno
-JOIN components comp ON sc.term = comp.term AND sc.lineno = comp.lineno AND sc.compname = comp.compname
-JOIN students s ON e.sid = s.sid
-GROUP BY e.term, e.lineno, e.sid, s.lname, s.fname;
-
--- Query the results from the view based on the term and lineno
-
-ACCEPT p_term_view CHAR PROMPT 'Enter the term for viewing course averages: '
-ACCEPT p_lineno_view NUMBER PROMPT 'Enter the lineno for viewing course averages: '
-
-SELECT * FROM student_course_avg_view
-WHERE term = :p_term_view
-AND lineno = :p_lineno_view;
+SELECT
+    DISTINCT sc.term                                                                                  AS "Term",
+    sc.lineno                                                                                AS "Line No",
+    sc.sid                                                                                   AS "Student ID",
+    s.lname                                                                                  AS "Last Name",
+    s.fname                                                                                  AS "First Name",
+    sc.compname                                                                              AS "Component Name",
+    com.weight                                                                               AS "Weight",
+    sc.points                                                                                AS "Points",
+    com.maxpoints                                                                            AS "Max Points",
+    (sc.points/com.maxpoints)*com.weight                                                     AS "Weighted Score",
+    SUM((SC.POINTS/COM.MAXPOINTS)*COM.WEIGHT) OVER (PARTITION BY sc.term, sc.lineno, sc.sid) AS "Course Average"
+FROM
+    students   s
+    JOIN scores sc
+    ON s.sid = sc.sid
+    JOIN components com
+    ON sc.term = com.term AND
+    sc.lineno = com.lineno AND
+    sc.compname = com.compname
+WHERE
+    sc.sid = 6666
+GROUP BY
+    sc.term,
+    sc.lineno,
+    sc.sid,
+    s.lname,
+    s.fname,
+    sc.compname,
+    com.weight,
+    sc.points,
+    com.maxpoints
+ORDER BY
+    sc.term,
+    sc.lineno,
+    sc.sid,
+    sc.compname;
